4b1ab1ee5357eae7c4d942d37da9e00fdefbc03d
Luke Winship <luke.winship@googlemail.com>
2016-03-05 16:04:23 +0000

    Updated to modern version of babel

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,34 +1,45 @@
-var fs = require('fs');
-var gulp = require('gulp');
-var browserify = require('browserify');
-var babelify = require('babelify');
+var fs = require('fs')
+var gulp = require('gulp')
+var browserify = require('browserify')
+var babelify = require('babelify')
+var runSequence = require('run-sequence')
 
 var PATHS = {
-    js: {
-        src: './src/contentScript.js',
-        dist: './dist/contentScript.js'
-    }
-};
-
-var buildJs = function() {
-    (browserify({debug: true})
-        .transform(babelify)
-        .require(PATHS.js.src, {entry: true})
-        .bundle()
-        .on('error', function(err) { console.log('Error:', err.message); })
-        .pipe(fs.createWriteStream(PATHS.js.dist))
-    );
-};
-
-var build = function() {
-    buildJs();
+  js: {
+    src: './src/contentScript.js',
+    dist: './dist/contentScript.js'
+  }
 }
 
-var watch = function() {
-    build();
-    gulp.watch(PATHS.js.src, ['build']);
-};
+var build = function(isDev) {
+  return function() {
+    return browserify({debug: isDev})
+     .transform(babelify)
+     .require(PATHS.js.src, {entry: true})
+     .bundle()
+     .on('error', function(err) {
+       console.error('Error:', err.message)
+     })
+     .pipe(fs.createWriteStream(PATHS.js.dist))
+  }
+}
 
-gulp.task('build', build);
-gulp.task('watch', watch);
-gulp.task('default', ['watch']);
+var justWatch = function() {
+  return gulp.watch(PATHS.js.src, ['build-dev'])
+}
+
+var watchAndBuild = function(cb) {
+  runSequence(
+    'build-dev',
+    'just-watch',
+    cb
+  )
+}
+
+gulp.task('build-dev', build(true))
+gulp.task('just-watch', justWatch)
+gulp.task('dev', watchAndBuild)
+
+gulp.task('prod', build(false))
+
+gulp.task('default', ['dev'])
--- src/contentScript.js
+++ src/contentScript.js
@@ -1,63 +1,63 @@
-import 'babel/polyfill';
-import {compose, concat, range, reduce} from 'ramda';
-import {getColour} from './colours';
+import {compose, concat, range, reduce} from 'ramda'
+import {getColour} from './colours'
 
-const charCodeRangeToLetters = (from, total) => range(from, from + total).map(n => String.fromCharCode(n));
-const concatAll = reduce(concat, []);
+const charCodeRangeToLetters = (from, total) => range(from, from + total).map(n => String.fromCharCode(n))
+const concatAll = reduce(concat, [])
 
-const LOWERCASE_LETTERS = charCodeRangeToLetters(97, 26);
-const UPPERCASE_LETTERS = charCodeRangeToLetters(65, 26);
-const DIGITS = charCodeRangeToLetters(48, 10);
+const LOWERCASE_LETTERS = charCodeRangeToLetters(97, 26)
+const UPPERCASE_LETTERS = charCodeRangeToLetters(65, 26)
+const DIGITS = charCodeRangeToLetters(48, 10)
 // Tags of elements whose text not to replace
-const TAG_BLACKLIST = (new Set()).add('script').add('noscript').add('style');
+const TAG_BLACKLIST = new Set(['script', 'noscript', 'style'])
 
 // Set of glyphs that this extension colours
-const SYNESTHESIFIABLE_GLYPHS = new Set(concatAll([LOWERCASE_LETTERS, UPPERCASE_LETTERS, DIGITS]));
+const SYNESTHESIFIABLE_GLYPHS = new Set(concatAll([LOWERCASE_LETTERS, UPPERCASE_LETTERS, DIGITS]))
 
 const synesthesifyGlyph = glyph => {
-    const element = document.createElement('span');
-    element.style.color = getColour(glyph);
-    element.textContent = glyph;
-    return element;
-};
+  const element = document.createElement('span')
+  element.style.backgroundColor = 'white'
+  element.style.color = getColour(glyph)
+  element.textContent = glyph
+  return element
+}
 
 const synesthesifyNode = textNode => {
-    const replacedContents = document.createDocumentFragment();
-    let unsynesthesifiableBuffer = '';
-    // Add punctuation / whitespace / etc as a raw text node
-    const addUnsynesthesifiableBufferAsTextNode = () => {
-        if (unsynesthesifiableBuffer.length !== 0) {
-            replacedContents.appendChild(document.createTextNode(unsynesthesifiableBuffer));
-            unsynesthesifiableBuffer = '';
-        }
-    };
-    for (let glyph of textNode.textContent) {
-        if (SYNESTHESIFIABLE_GLYPHS.has(glyph)) {
-            addUnsynesthesifiableBufferAsTextNode();
-            replacedContents.appendChild(synesthesifyGlyph(glyph));
-        } else {
-            unsynesthesifiableBuffer += glyph;
-        }
+  const replacedContents = document.createDocumentFragment()
+  let unsynesthesifiableBuffer = ''
+  // Add punctuation / whitespace / etc as a raw text node
+  const addUnsynesthesifiableBufferAsTextNode = () => {
+    if (unsynesthesifiableBuffer.length !== 0) {
+      replacedContents.appendChild(document.createTextNode(unsynesthesifiableBuffer))
+      unsynesthesifiableBuffer = ''
     }
-    addUnsynesthesifiableBufferAsTextNode();
-    textNode.parentNode.replaceChild(replacedContents, textNode);
-};
+  }
+  for (let glyph of textNode.textContent) {
+    if (SYNESTHESIFIABLE_GLYPHS.has(glyph)) {
+      addUnsynesthesifiableBufferAsTextNode()
+      replacedContents.appendChild(synesthesifyGlyph(glyph))
+    } else {
+      unsynesthesifiableBuffer += glyph
+    }
+  }
+  addUnsynesthesifiableBufferAsTextNode()
+  textNode.parentNode.replaceChild(replacedContents, textNode)
+}
 
 const walk = rootNode => {
-    // Make changes to offline copy of root node so as not to incur wrath of constant DOM reflow
-    const copiedRootNode = rootNode.cloneNode(true);
-    const walker = document.createTreeWalker(copiedRootNode, NodeFilter.SHOW_TEXT, null, false);
-    const nodes = [];
-    while (walker.nextNode()) {
-        if (walker.currentNode.textContent.trim().length === 0) continue;
-        // Make sure we don't touch script / style / etc tags
-        if (TAG_BLACKLIST.has(walker.currentNode.parentNode.tagName.toLowerCase())) continue;
-        nodes.push(walker.currentNode);
-    }
-    for (let node of nodes) {
-        synesthesifyNode(node);
-    }
-    rootNode.parentNode.replaceChild(copiedRootNode, rootNode);
-};
+  // Make changes to offline copy of root node so as not to incur wrath of constant DOM reflow
+  const copiedRootNode = rootNode.cloneNode(true)
+  const walker = document.createTreeWalker(copiedRootNode, NodeFilter.SHOW_TEXT, null, false)
+  const nodes = []
+  while (walker.nextNode()) {
+    if (walker.currentNode.textContent.trim().length === 0) { continue }
+    // Make sure we don't touch script / style / etc tags
+    if (TAG_BLACKLIST.has(walker.currentNode.parentNode.tagName.toLowerCase())) { continue }
+    nodes.push(walker.currentNode)
+  }
+  for (let node of nodes) {
+    synesthesifyNode(node)
+  }
+  rootNode.parentNode.replaceChild(copiedRootNode, rootNode)
+}
 
-walk(document.body);
+walk(document.body)
